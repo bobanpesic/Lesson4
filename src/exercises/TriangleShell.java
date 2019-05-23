e306340258098bbdb788cd88672b063843b42fa0
bobanpesic <bobanpesic10gmail.com>
2019-05-23 20:18:54 +0200

    change

--- src/exercises/TriangleShell.java
+++ src/exercises/TriangleShell.java
@@ -1,35 +1,37 @@
 package exercises;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class TriangleShell {
 
-	// 1. Instantiate a new Robot
+	
+	Robot boby = new Robot();
 
 	void go() {
-		drawTriangle(100);	//3. Remove this command, it's just for testing
+		
+		boby.setSpeed(200);
 
-		// 6. Make the robot go as fast as possible
+		int length = 50;
 
-		// 4. make a variable to hold the length of the triangle and set it to 50
-
-		// 7. Do all the steps below, 60 times
-
-			// 9. Change the color of the pen to a random color
+		for (int i = 0; i < 60; i++) {
+			boby.setRandomPenColor();
+			length += 10;
+			drawTriangle(length);
+			boby.turn(6);
+		}
 	
-			// 8. Increase the length of the side by 10 pixels
-	
-			// 5. call your drawTriangle() method using your length variable
-
-			// 10. Turn the robot 6 degrees to the right
-
 	}
 
-	/* 2. Fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
 	private void drawTriangle(int length) {
+		boby.penDown();
 		
-		
+		for (int i = 0; i < 3; i++) {
+			boby.move(length);
+			boby.turn(120);
+		}
+
 	}
 
-	
 	public static void main(String[] args) {
 		new TriangleShell().go();
 	}
